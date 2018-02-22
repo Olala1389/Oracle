3426a1d200de60a3f2a7bcec470bc63ebc760629
Olga Makarova <olga.alex.makar@gmail.com>
2018-02-22 14:57:53 -0800

    added some more Java code

---
+++ JavaProject/AnimalPictureEscape_program/AnimalPictureEscape.java
@@ -0,0 +1,10 @@
+public class AnimalPictureEscape {
+    public static void main(String[] args){ 
+        //System.out.println("\'\t\'");
+        System.out.println("\t   \'__\'");
+        System.out.println("\t   (oo)");
+        System.out.println("  +=========\\/");
+        System.out.println(" / || %%% ||");
+        System.out.println("*  ||-----||");
+    }
+}
